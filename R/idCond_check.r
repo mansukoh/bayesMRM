4805cc629018f685f1b197dc1e9e848f27d08ad9
mansukoh <mansuk55@gmail.com>
2020-10-16 16:18:36 +0900

    ..

--- R/idCond_check.r
+++ R/idCond_check.r
@@ -2,12 +2,12 @@
 #' @description  Check the identifiability conditions C1-C2 of Park and Oh (2015).
 #' @title Check the identifiability conditions
 #' @usage idCond_check(P)
-#' @param P  the source composition matrix in multivariate receptor model 
+#' @param P  the source composition matrix in multivariate receptor model
 
 
 idCond_check = function(P){
   q = base::nrow(P)
-  
+
   idCond_1 = 1
   idCond_2 = 1
   for (k in 1:q){
@@ -15,11 +15,10 @@ idCond_check = function(P){
     idCond_1 = idCond_1*(base::length(idCol)  >= q-1)  # Condition C1
     idRow = base::c(1:q)[-k]
     P_k = P[idRow, idCol]
-    #idCond_2 = idCond_2*(base::qr(P_k)$rank  == q-1)   # Condition C2
-    idCond_2 = idCond_2*(abs(base::eigen(P_k)$values[1]/base::eigen(P_k)$values[q-1 ]) < 200 )  # Condition C2
+    idCond_2 = idCond_2*(base::qr(P_k)$rank  == q-1)   # Condition C2
   }
-  
-  idCond = idCond_1 * idCond_2 
-  
+
+  idCond = idCond_1 * idCond_2
+
   return(idCond) # if all the conditions are satisfied, it is TRUE (1).
 }
